165f2de0d74a6dd09aefacbd2815c2822bde0918
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-03-14 18:50:16 +0000

    deploy: c370ddef71e9f8defc9e9cfc872ad911bf84aef8

--- code.js
+++ code.js
@@ -2,3 +2,25 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+let snapCrackle = function (maxValue) {
+    let message;
+    let number = 1;
+    while (number <= maxValue) {
+        if (number % 2 !== 0) {
+            if (number % 5 === 0) {
+                message = "SnapCrackle, ";
+            } else {
+            message = "Snap, ";
+            }
+        }
+        else if (number % 5 === 0) {
+            message = "Crackle, ";
+        } else {
+            message = number + ", ";
+        }
+        number++;
+    }
+    return message;
+}
+
+console.log(snapCrackle(12));
